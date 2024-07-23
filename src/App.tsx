cad5fdf2c688548a95c29f3c983db0aff0acecfc
Ariel <ariel.boucheikh@gmail.com>
2024-07-22 22:30:42 -0400

    Add dev tooling for FE

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,90 @@
 import { useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
-//require('purecss')
+import '@mantine/core/styles.css';
+import { MantineProvider, AppShell, Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+
+interface HeaderArgs {
+    burgerOpen: boolean;
+    burgerToggle: () => void;
+}
+
+function Header({ burgerOpen, burgerToggle }: HeaderArgs) {
+    return (
+        <Burger
+            opened={burgerOpen}
+            onClick={burgerToggle}
+            hiddenFrom="sm"
+            size="sm"
+        />
+    );
+}
+
+function Navbar() {
+    return "";
+}
+
+function ContentPane() {
+    const [greetMsg, setGreetMsg] = useState("");
+    const [name, setName] = useState("");
+
+    async function greet() {
+        // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+        setGreetMsg(await invoke("greet", { name }));
+    }
+
+    return (
+        <div className="container">
+            <h1>Home</h1>
+            <form
+                className="row"
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    greet();
+                }}
+            >
+                <input
+                    id="greet-input"
+                    onChange={(e) => setName(e.currentTarget.value)}
+                    placeholder="Enter a name..."
+                />
+                <button type="submit">Greet</button>
+            </form>
+            <p>{greetMsg}</p>
+        </div>
+    );
+}
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+    const [navBarOpen, navBarHandlers] = useDisclosure();
+    return (
+        <MantineProvider defaultColorScheme="dark">
+            <AppShell
+                header={{ height: 60 }}
+                navbar={{
+                    width: 300,
+                    breakpoint: 'sm',
+                    collapsed: { mobile: !navBarOpen },
+                }}
+                padding="md"
+            >
+                <AppShell.Header>
+                    <Header
+                        burgerOpen={navBarOpen}
+                        burgerToggle={navBarHandlers.toggle}
+                    />
+                </AppShell.Header>
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
+                <AppShell.Navbar p="md">
+                    <Navbar/>
+                </AppShell.Navbar>
 
-  return (
-    <div className="container">
-      <h1>Landing Page</h1>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
-    </div>
-  );
+                <AppShell.Main>
+                    <ContentPane/>
+                </AppShell.Main>
+            </AppShell>
+        </MantineProvider>
+    );
 }
 
 export default App;
