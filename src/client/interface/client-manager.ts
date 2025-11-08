1ceed13382123ca45f674b1b4588f4005cb6365e
Ariel <ariel.boucheikh@gmail.com>
2025-11-08 01:27:42 -0500

    Add events to header manager

--- src/client/interface/client-manager.ts
+++ src/client/interface/client-manager.ts
@@ -1,22 +1,5 @@
-import { Id } from "@/interface";
-import { ProjectResponse, DomainManager, EntityType } from "@/domain";
-
-export interface OpenEntryCreatorArguments {
-    entityType?: EntityType;
-    folderId?: Id;
-}
+import { DomainManager } from "@/domain";
 
 export interface IClientManager {
     domain: DomainManager;
-
-    openHome(): void;
-    openSettings(): void;
-    openProjectCreator(): void;
-    openEntryCreator(args?: OpenEntryCreatorArguments): void;
-    createProject(
-        name: string,
-        dbFilePath: string,
-    ): Promise<ProjectResponse | null>;
-    loadProject(): Promise<ProjectResponse | null>;
-    closeProject(): Promise<boolean>;
 }
