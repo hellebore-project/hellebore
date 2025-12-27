57082b725144b7bfb7a794f6416da73c511766e9
Ariel <ariel.boucheikh@gmail.com>
2025-12-26 23:23:01 -0500

    Regenerate BE entity models

--- src-tauri/migration/src/init/person.rs
+++ src-tauri/migration/src/init/person.rs
@@ -3,6 +3,7 @@ use sea_orm_migration::{prelude::*, schema::*};
 use crate::init::entry::Entry;
 
 const PERSON_ENTRY_ID_FK_NAME: &str = "fk_person_entry_id";
+const PERSON_ENTRY_ID_INDEX_NAME: &str = "index_person_entry_id";
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
@@ -28,10 +29,25 @@ impl MigrationTrait for Migration {
                     .to_owned(),
             )
             .await?;
+
+        manager
+            .create_index(
+                Index::create()
+                    .name(PERSON_ENTRY_ID_INDEX_NAME)
+                    .table(Person::Table)
+                    .col(Person::EntryId)
+                    .unique()
+                    .to_owned(),
+            )
+            .await?;
+
         Ok(())
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_index(Index::drop().name(PERSON_ENTRY_ID_INDEX_NAME).to_owned())
+            .await?;
         manager
             .drop_foreign_key(ForeignKey::drop().name(PERSON_ENTRY_ID_FK_NAME).to_owned())
             .await?;
