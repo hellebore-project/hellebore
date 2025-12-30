0fe8295600d39511d83abd14f3189942e8451b34
Ariel <ariel.boucheikh@gmail.com>
2025-12-29 23:54:29 -0500

    Simplify error handler in the FE

--- src/domain/schema/error.ts
+++ src/domain/schema/error.ts
@@ -1,18 +1,5 @@
-import { EntityType } from "@/domain/constants";
-
 export enum DomainErrorType {
     UNKNOWN_ERROR = "UnknownError",
-    DATABASE_CONNECTION_FAILED = "DatabaseConnectionFailed",
-    DATABASE_MIGRATION_FAILED = "DatabaseMigrationFailed",
-    NOT_INSERTED = "NotInserted",
-    NOT_UPDATED = "NotUpdated",
-    NOT_FOUND = "NotFound",
-    NOT_DELETED = "NotDeleted",
-    QUERY_FAILED = "QueryFailed",
-    FIELD_NOT_UPDATED = "FieldNotUpdated",
-    FIELD_NOT_UNIQUE = "FieldNotUnique",
-    FIELD_INVALID = "FieldInvalid",
-    PROJECT_NOT_LOADED = "ProjectNotLoaded",
 }
 
 export type BackendApiError = Record<DomainErrorType, object>;
@@ -24,52 +11,3 @@ export interface BaseDomainError {
 export interface DomainMessageError extends BaseDomainError {
     msg: string;
 }
-
-export interface DatabaseConnectionFailedError extends DomainMessageError {
-    connectionString: string;
-}
-
-export interface DatabaseMigrationFailedError extends DomainMessageError {
-    connectionString: string;
-}
-
-export interface NotInsertedError extends DomainMessageError {
-    entityType: EntityType;
-}
-
-export interface NotUpdatedError extends DomainMessageError {
-    entityType: EntityType;
-}
-
-export interface NotFoundError extends DomainMessageError {
-    entityType: EntityType;
-}
-
-export interface NotDeletedError extends DomainMessageError {
-    entityType: EntityType;
-}
-
-export interface QueryFailedError extends DomainMessageError {
-    entityType: EntityType;
-}
-
-export interface FieldNotUpdatedError extends DomainMessageError {
-    entityType: EntityType;
-    key: string;
-}
-
-export interface FieldNotUniqueError extends BaseDomainError {
-    entityType: EntityType;
-    id?: number;
-    key: string;
-    value: string;
-}
-
-export interface FieldInvalidError extends DomainMessageError {
-    entityType: EntityType;
-    id?: number;
-    key: string;
-    value: string;
-}
-
-export type ProjectNotLoadedError = BaseDomainError;
--- src/domain/services/error-handler.ts
+++ src/domain/services/error-handler.ts
@@ -1,20 +1,14 @@
-import { EntityType } from "@/domain/constants";
 import {
     BackendApiError,
     BaseDomainError,
     DomainErrorType,
     DomainMessageError,
-    FieldNotUniqueError,
-    FieldNotUpdatedError,
-    NotUpdatedError,
 } from "@/domain/schema";
 
 export function process_backend_api_error(
     api_error: BackendApiError,
 ): BaseDomainError {
-    const keys = Object.keys(api_error).filter((key) =>
-        Object.values(DomainErrorType).includes(key as DomainErrorType),
-    );
+    const keys = Object.keys(api_error);
     if (keys.length == 0)
         return {
             type: DomainErrorType.UNKNOWN_ERROR,
@@ -23,36 +17,4 @@ export function process_backend_api_error(
 
     const type = keys[0] as DomainErrorType;
     return { type, ...api_error[type] };
-}
-
-export function is_table_not_updated(
-    error: BaseDomainError,
-    entityType: EntityType,
-): boolean {
-    if (error.type !== DomainErrorType.NOT_UPDATED) return false;
-
-    const _error = error as NotUpdatedError;
-    return _error.entityType == entityType;
-}
-
-export function is_field_non_unique(
-    error: BaseDomainError,
-    entityType: EntityType,
-    fieldName: string,
-): boolean {
-    if (error.type !== DomainErrorType.FIELD_NOT_UNIQUE) return false;
-
-    const _error = error as FieldNotUniqueError;
-    return _error.entityType == entityType && _error.key == fieldName;
-}
-
-export function is_field_not_updated(
-    error: BaseDomainError,
-    entityType: EntityType,
-    fieldName: string,
-): boolean {
-    if (error.type !== DomainErrorType.FIELD_NOT_UPDATED) return false;
-
-    const _error = error as FieldNotUpdatedError;
-    return _error.entityType == entityType && _error.key == fieldName;
 }
