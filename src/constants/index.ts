f565fbf5e8fa8387ffd7da5861ea60b39af7939f
Ariel <ariel.boucheikh@gmail.com>
2026-01-10 13:54:15 -0500

    Add domain subfolder to constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,1 @@
-export * from "./routes";
-export * from "./entity";
-export * from "./entry";
-export * from "./folder";
-export * from "./person";
-export * from "./word";
+export * from "./domain";
