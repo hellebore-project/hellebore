20ca23fb71a822c43eb19e2d0678bdc5d2aa50c0
Ariel Boucheikhchoukh <83319124+ari-bou@users.noreply.github.com>
2026-04-03 22:53:10 -0400

    Add svelte delete entry button (#145)

---
+++ src/ui/centre/entry-editor/delete-entry-button/delete-entry-button.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import TrashIcon from "@lucide/svelte/icons/trash";
+
+    import * as Tooltip from "@/lib/components/tooltip";
+    import { Button } from "@/lib/components/button";
+
+    import type { DeleteEntryButtonProps } from "./delete-entry-button-interface";
+
+    let { service }: DeleteEntryButtonProps = $props();
+</script>
+
+<Tooltip.Provider>
+    <Tooltip.Root>
+        <Tooltip.Trigger>
+            <Button
+                variant="outline"
+                size="icon-sm"
+                color="destructive"
+                onclick={() => service.deleteEntry()}
+            >
+                <TrashIcon />
+            </Button>
+        </Tooltip.Trigger>
+        <Tooltip.Content>
+            <p>Delete entry</p>
+        </Tooltip.Content>
+    </Tooltip.Root>
+</Tooltip.Provider>
---
+++ src/ui/centre/entry-editor/delete-entry-button/index.ts
@@ -0,0 +1,2 @@
+export * from "./delete-entry-button-interface";
+export { default as DeleteEntryButton } from "./delete-entry-button.svelte";
