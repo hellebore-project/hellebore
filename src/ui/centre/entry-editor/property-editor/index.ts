3f971604f9383d698fc35dd587a42b4a8f713416
Ariel <ariel.boucheikh@gmail.com>
2026-04-06 01:15:29 -0400

    Add more imports to property-editor index

--- src/ui/centre/entry-editor/property-editor/index.ts
+++ src/ui/centre/entry-editor/property-editor/index.ts
@@ -1,2 +1,4 @@
-export { default as PropertyEditor } from "./property-editor.svelte";
+export * from "./property-table";
+export * from "./property-editor-interface";
 export { PropertyEditorService } from "./property-editor-service.svelte";
+export { default as PropertyEditor } from "./property-editor.svelte";
